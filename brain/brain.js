13ec8cedbe00397e5d9224b7f72f2cb0173835fa
Piotr <p_bartoszak@wp.pl>
2019-11-24 20:59:59 +0100

    Start create neurons net

---
+++ brain/brain.js
@@ -0,0 +1,15 @@
+class Brain {
+    constructor(inputs, hidden, outputs) {
+        this.inputsLayers = createLayer(inputs);
+        this.hidden = hidden;
+        this.outputs = outputs;
+    }
+
+    createLayer (nodesNumber) {
+        
+    }
+
+    init() {
+
+    }
+}
---
+++ brain/neuron.js
@@ -0,0 +1,5 @@
+class Nauron {
+    constructor() {
+        this.weights = [];
+    }
+}
