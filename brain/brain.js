37dff9e7b2b1f4c72312ed0630850407c69fc8ca
Piotr <p_bartoszak@wp.pl>
2019-11-25 22:13:43 +0100

    Develope neurons net

--- brain/brain.js
+++ brain/brain.js
@@ -1,15 +1,35 @@
 class Brain {
-    constructor(inputs, hidden, outputs) {
-        this.inputsLayers = createLayer(inputs);
-        this.hidden = hidden;
-        this.outputs = outputs;
+    constructor(inputs, layer1, outputs) {
+        this.inputs = inputs;
+        this.layer1 = this.createLayer(inputs,layer1);
+        // this.layer2 = this.createLayer(layer1, layer2);
+        this.outputs = this.createLayer(layer1, outputs);
     }
 
-    createLayer (nodesNumber) {
+    createLayer (inputs, nodes) {
+        let layer = [];
+        for(let i = 0; i < nodes; i++) {
+            layer.push(new Neuron(inputs));
+        }
+        return layer;
+    }
+    
+    initRandomWeights() {
+        this.layer1 = this.prepareRandomWeightsForLayer(this.layer1);
+        this.outputs = this.prepareRandomWeightsForLayer(this.outputs);
+    }
+
+    prepareRandomWeightsForLayer(layer) {
+        for(let i = 0; i < layer.length; i++) {
+            const weights = layer[i].weights.length;
+            for(let j = 0; j < weights; j++) {
+                layer[i].weights[j] = Math.floor(Math.random()*10);
+            }
+        }
+        return layer;
+    }
+
+    deliverInputs(inputs) {
         
-    }
-
-    init() {
-
     }
 }
--- brain/neuron.js
+++ brain/neuron.js
@@ -1,5 +1,21 @@
-class Nauron {
-    constructor() {
-        this.weights = [];
+class Neuron {
+    constructor(inputs) {
+        this.weights = this.createInputs(inputs);
+    }
+
+    createInputs(inputs) {
+        let weights = []
+        for(let i = 0; i < inputs; i++) {
+            weights.push(0);
+        }
+        return weights;
+    }
+
+    calcOutput(inputs) {
+        let sum = 0;
+        for(let i = 0; i < this.weights.length; i++) {
+            sum += this.weights[i] * inputs[i];
+        }
+        return sum;
     }
 }
