41ed399c54ace6471446b44ca087b141f62bc7e2
Piotr <p_bartoszak@wp.pl>
2019-12-07 19:07:27 +0100

    Add score text to progress bar

--- chartService.js
+++ chartService.js
@@ -3,11 +3,13 @@ class ChartService {
     constructor(x,y) {
         this.x = x;
         this.y = y;
+        this.scoreText = 'no data';
         this.score = 0;
     }
 
     calculateScore(actualCP, allCPs) {
         this.score = Math.floor(actualCP / allCPs * 100);
+        this.scoreText = actualCP + '/' + allCPs + '    ' + this.score + '%';
     }
 
     draw() {
@@ -24,6 +26,11 @@ class ChartService {
         strokeWeight(4);
         stroke(51);
         rect(0,0,100, 20);
+        fill(50);
+        strokeWeight(1);
+        textFont('Helvetica');
+        textSize(16);
+        text(this.scoreText, 0, 40);
         pop();
     }
 }
